1dc1d8c248781d1c027d052294abba203fb9e5b0
Oscar Johansson <1994.oscar.j@gmail.com>
2017-11-10 22:56:14 +0100

    Add player selection

--- src/create.js
+++ src/create.js
@@ -1,17 +1,25 @@
 function createState(game){
   game.physics.startSystem(Phaser.Physics.ARCADE);
+  console.log(game.input);
   game.playerGroup = game.add.group();
   game.waterGroup = game.add.group();
   game.add.image(game.world.centerX, game.world.centerY, 'background').anchor.set(0.5);
+  game.selection = null;
   game.map.forEach(function(o) {
     var object = game.add.sprite(o.x, o.y, 'cactus');
     object.tint = getColor(o.owner);
+    object.owner = o.owner;
     object.scale.setTo(o.scale, o.scale);
     object.units = o.units;
     object.reg = o.reg;
+    object.inputEnable = true;
     object.anchor.setTo(0.5, 0.5);
     object.text = game.add.text(o.x, o.y, o.units, { font: "15px Arial", fill: "#ffffff",stroke: '#000000', strokeThickness: 5});
     object.text.anchor.setTo(0.5,0);
     game.playerGroup.add(object);
+    game.playerGroup.add(object.text);
   });
+  game.timer = game.time.create(false);
+  game.timer.loop(500, function(){updateUnits(game);}, this);
+  game.timer.start();
 }
--- src/update.js
+++ src/update.js
@@ -1,8 +1,37 @@
 function updateState(game){
   game.world.bringToTop(game.playerGroup);
   game.world.bringToTop(game.waterGroup);
+  if(game.input.activePointer.isDown) {
+      game.mouseDown = true;
     game.playerGroup.forEach(function(p) {
-        p.units += p.reg;
-        p.text.text = p.units;
+      if((p.getBounds().contains(game.input.activePointer.x, game.input.activePointer.y))) {
+        if(p.owner == ownerEnum.PLAYER) {
+          p.alpha = 0.5;
+        } 
+      }
     });
+  }
+  if(!game.input.activePointer.isDown && game.mouseDown) {
+    game.mouseDown = false;
+    game.playerGroup.forEach(function(p) {
+      if(p.owner === ownerEnum.PLAYER) {
+        p.alpha = 1;
+      }
+    });
+    game.playerGroup.forEach(function(p) {
+      if(p.getBounds().contains(game.input.activePointer.x, game.input.activePointer.y) && p.owner !== ownerEnum.PLAYER) {
+        //send units
+      }
+
+
+  });
+  }    
+}
+
+
+function updateUnits(game) {
+  game.playerGroup.forEach(function(p) {
+      p.units += p.reg;
+      p.text.text = p.units;
+  });
 }
