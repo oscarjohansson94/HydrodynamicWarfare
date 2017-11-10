1eb24a4510af17a9c99d8a765ec15ff66e56dfa9
Oscar Magnusson <balobba@gmail.com>
2017-11-10 22:32:13 +0100

    Add water image to game

--- src/preload.js
+++ src/preload.js
@@ -1,4 +1,5 @@
 function preloadState(game){
     game.load.image('cactus', '../assets/cactus.png');
+    game.load.image('water', '../assets/water.png');
     game.load.image('background', '../assets/background.png');
 }
