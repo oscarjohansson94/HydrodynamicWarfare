5fd3a96ae69b5f7689c2e15f2617569210a45059
Oscar Magnusson <balobba@gmail.com>
2017-11-11 10:34:18 +0100

    Add water line to game

--- src/preload.js
+++ src/preload.js
@@ -1,5 +1,6 @@
 function preloadState(game){
     game.load.image('cactus', '../assets/cactus.png');
     game.load.image('water', '../assets/water.png');
+    game.load.image('line', '../assets/line.png');
     game.load.image('background', '../assets/background.png');
 }
